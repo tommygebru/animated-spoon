d2188447104a2f8bff532b48f557e208e34c3643
tommygebru <gebru.t@gmail.com>
2020-05-20 13:54:57 -0700

    set default value

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ $("document").ready(function() {
 	/********************************
 	R A N D O M   F E A T U R E
 	*********************************/
+  $("#dog-amount").val(3);
+  	$("#randomArticle aside").hide();
 });//READY
