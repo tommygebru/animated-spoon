d42e89cb2595deff10f9f1280a9e8720f260a1c2
tommygebru <gebru.t@gmail.com>
2020-05-21 22:39:53 -0700

    add search feature

--- index.js
+++ index.js
@@ -1,43 +1,87 @@
 $("document").ready(function() {
 	/********************************
-	R A N D O M   F E A T U R E
+	C L I C K  E V E N T S
 	*********************************/
-  $("#dog-amount").val(3);
-  	$("#randomArticle aside").hide();
-    $("#randomSection").on("click", "#randomButton", function(event) {
+	$("#dog-amount").val(3);
+	$("#randomSection").on("click", "#randomButton", function(event) {
 		event.preventDefault();
-		$("#randomArticle aside").show();
 		$("#randomArticle img").remove();
 		//remove images
-    let dogNum = $("#dog-amount").val();
-		$("#dogNum").text(dogNum);
-		pullDogImages(dogNum);
+		let dogAmount = $("#dog-amount").val();
+		$("#dogAmount").text(dogAmount);
+		if (dogAmount > 0 && dogAmount < 51) {
+			pullRandomDogImages(dogAmount);
+		}//if
 		//function
-	}); //event
-  function pullDogImages(dogNum) {
-    fetch(`https://dog.ceo/api/breeds/image/random/${Number(dogNum)}`)
+	}); //event__Random
+	$("#searchSection").on("click", "#searchButton", function(event) {
+		event.preventDefault();
+		$("#searchArticle img").remove();
+		//remove images
+		let dogSearch = $("#dog-search").val().toLowerCase();
+		$("#dogSearch").text(dogSearch);
+		pullSearchDogImages(dogSearch);
+		//function
+	}); //event__Search
+	/********************************
+	P U L L   D O G  I M A G E S
+	*********************************/
+	function pullSearchDogImages(dogSearch) {
+		fetch(`https://dog.ceo/api/breed/${dogSearch}/images/random`)
     .then(response => {
 			if (response.ok) {
 				return response.json();
 			} else {
 				throw console.log(response.text());
 			}
-		}).then(responseJson => displayResults(responseJson))
-    .catch(error => displayError(error));
-	} //function
-});//READY
-function displayResults(responseJson) {
+		}).then(responseJson => displaySearchResults(responseJson))
+      .catch(error => displaySearchError(error));
+	} //function__search
+	function pullRandomDogImages(dogAmount) {
+		fetch(`https://dog.ceo/api/breeds/image/random/${Number(dogAmount)}`)
+    .then(response => {
+			if (response.ok) {
+				return response.json();
+			} else {
+				throw console.log(response.text());
+			}
+		}).then(responseJson => displayRandomResults(responseJson))
+      .catch(error => displayRandomError(error));
+	} //function__random
+}); //READY
+/********************************
+D I S P L A Y   R E S U L T S
+*********************************/
+function displaySearchResults(responseJson) {
 	console.log(responseJson);
-	let dogNum = responseJson.message.length;
-	if (dogNum) {
-		for (let i = 0; i < dogNum; i++) {
-			console.log(dogNum, i);
+	let dogList = responseJson.message.length;
+	if (dogList) {
+		for (let i = 1; i <= 1; i++) {
+			console.log(dogList, i, responseJson.message);
+			$("#searchArticle aside").prepend(`<img src=${responseJson.message} />`);
+		}
+	} else {
+		console.log("check your inputs");
+	}
+}; //function__search
+function displayRandomResults(responseJson) {
+	console.log(responseJson);
+	let dogList = responseJson.message.length;
+	if (dogList) {
+		for (let i = 0; i < dogList; i++) {
+			console.log(dogList, i, responseJson.message);
 			$("#randomArticle aside").prepend(`<img src=${responseJson.message[i]} />`);
 		}
 	} else {
 		console.log("check your inputs");
 	}
-}; //function
-function displayError(error) {
+}; //function__random
+/********************************
+D I S P L A Y   E R R O R S
+*********************************/
+function displaySearchError(error) {
+	alert("ERROR 404: Dog Breed Not Found");
+} //function__search
+function displayRandomError(error) {
 	alert("ERROR 404: Images not found");
-}
+} //function__random
